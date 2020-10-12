b1b738212506fac13cd306f602aff5e7e6d407df
ecnusgn <sjtusgn@163.com>
2020-10-12 21:33:34 +0800

    new 21

--- 837_new_21.py
+++ 837_new_21.py
@@ -1,11 +1,17 @@
 def new_21(n, k, w):
-    dp = [0] * w
-    dp[-1] = (n-k+1)/w
-    for i in range(w-2, -1, -1):
-        dp[i] = (1/w * dp[i+1]) + (dp[i+1]-)
+        if k == 0:
+            return 1
+        dp = [0] * (k+w)
+        for i in range(k, min(n, k+w-1)+1):
+            dp[i] = 1
+        for i in range(k-1, -1, -1):
+            for j in range(1, w+1):
+                dp[i] += dp[i+j]/w
+        return dp[0]
         
 
 if __name__ == "__main__":
     n = 21
     k = 17
     w = 10
+    print(new_21(n, k, w))
