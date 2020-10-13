b1464c1649a789167e615980b48353b55d360e86
binkesi <sjtusgn@163.com>
2020-10-13 14:51:14 +0800

    drop egg recursive and memo

---
+++ 887drop_egg.py
@@ -0,0 +1,28 @@
+def drop_egg(K, N):
+    memo = {}
+    def dp(k, n):
+        if (k, n) not in memo:
+            if n == 0:
+                ans = 0
+            elif k == 1:
+                ans = n
+            else:
+                lo, hi = 1, n
+                while lo + 1 < hi:
+                    x = (lo+hi) // 2
+                    t1 = dp(k-1, x-1)
+                    t2 = dp(k, n-x)
+                    if t1 < t2:
+                        lo = x
+                    elif t1 > t2:
+                        hi = x
+                    else:
+                        lo = hi = x
+                ans = 1 + min(max(dp(k-1, x-1), dp(k, n-x)) for x in (lo, hi))
+            memo[k, n] = ans
+        return memo[k, n]
+    return dp(K, N)
+if __name__ == "__main__":
+    N = 10
+    K = 3
+    print(drop_egg(K, N))
